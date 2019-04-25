70e5c00ba23ac4ba4e803861769de93bf80d48b2
arthur <gogoke1@naver.com>
2019-04-26 00:58:36 +0900

    fr

---
+++ src/main/java/kr/nameis/name/controller/TestController.java
@@ -0,0 +1,12 @@
+package kr.nameis.name.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+}
